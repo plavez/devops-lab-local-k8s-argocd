2b2c431cdd1580fe9a02612179bbdbaaed2956c7
Vladislav <plavez@gmail.com>
2025-08-31 23:43:52 +0200

    Add Prometheus and Grafana

--- main.tf
+++ main.tf
@@ -5,7 +5,6 @@ terraform {
   }
 }
 
-
 resource "helm_release" "argocd" {
   name             = "argocd-${var.cluster_name}"
   namespace        = "argocd-${var.cluster_name}"
@@ -15,37 +14,103 @@ resource "helm_release" "argocd" {
   chart      = "argo-cd"
   version    = var.chart_version
 
+
+
   values  = [file("${path.module}/argocd-dev.yaml")]
   timeout = 600
   wait    = true
 }
 
+resource "helm_release" "metrics_server" {
+  name       = "metrics-server"
+  namespace  = "kube-system"
+  repository = "https://kubernetes-sigs.github.io/metrics-server/"
+  chart      = "metrics-server"
+  version    = "3.12.2"
 
+  set {
+    name  = "args"
+    value = "{--kubelet-insecure-tls,--kubelet-preferred-address-types=InternalIP,Hostname,InternalDNS,ExternalDNS,ExternalIP}"
+  }
+}
 
-# # ArgoCD в DEV
-# resource "helm_release" "argocd_dev" {
-#   provider         = helm.dev
-#   name             = "argocd-dev"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd-dev"
-#   version          = var.chart_version
-#   create_namespace = true
-#   values           = [file("${path.module}/argocd-dev.yaml")]
-#   timeout          = 600
-#   wait             = true
+# resource "kubernetes_namespace" "monitoring_dev" {
+#   provider = kubernetes.dev
+#   metadata {
+#     name = "monitoring"
+#   }
 # }
 
-# # ArgoCD в PROD
-# resource "helm_release" "argocd_prod" {
-#   provider         = helm.prod
-#   name             = "argocd-prod"
-#   repository       = "https://argoproj.github.io/argo-helm"
-#   chart            = "argo-cd"
-#   namespace        = "argocd-prod"
-#   version          = var.chart_version
+# ## Kubernetes Dashboard
+# resource "helm_release" "dashboard" {
+#   name             = "kubernetes-dashboard"
+#   namespace        = "kubernetes-dashboard"
 #   create_namespace = true
-#   values           = [file("${path.module}/argocd-prod.yaml")]
-#   timeout          = 600
-#   wait             = true
+
+#   repository = "https://kubernetes.github.io/dashboard/"
+#   chart      = "kubernetes-dashboard"
+#   version    = "7.12.0"
+
+
+#   set {
+#     name  = "serviceAccount.create"
+#     value = true
+#   }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "dashboard-sa"
+#   }
+# }
+
+# resource "kubernetes_service_account" "dash_editor" {
+#   metadata {
+#     name      = "dash-editor"
+#     namespace = "kubernetes-dashboard"
+#   }
+#   depends_on = [helm_release.dashboard]
+# }
+
+# resource "kubernetes_cluster_role_binding" "dash_editor" {
+#   metadata {
+#     name = "dash-editor-binding"
+#   }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = "edit"
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = kubernetes_service_account.dash_editor.metadata[0].name
+#     namespace = "kubernetes-dashboard"
+#   }
+# }
+
+# resource "kubernetes_cluster_role" "dash_cluster_read" {
+#   metadata { name = "dash-cluster-read" }
+#   rule {
+#     api_groups = [""]
+#     resources  = ["nodes", "namespaces"]
+#     verbs      = ["get", "list", "watch"]
+#   }
+#   # (опционально, чтобы работали метрики в обзоре)
+#   rule {
+#     api_groups = ["metrics.k8s.io"]
+#     resources  = ["nodes", "pods"]
+#     verbs      = ["get", "list", "watch"]
+#   }
+# }
+
+# resource "kubernetes_cluster_role_binding" "dash_cluster_read" {
+#   metadata { name = "dash-cluster-read-binding" }
+#   role_ref {
+#     api_group = "rbac.authorization.k8s.io"
+#     kind      = "ClusterRole"
+#     name      = kubernetes_cluster_role.dash_cluster_read.metadata[0].name
+#   }
+#   subject {
+#     kind      = "ServiceAccount"
+#     name      = kubernetes_service_account.dash_editor.metadata[0].name
+#     namespace = kubernetes_service_account.dash_editor.metadata[0].namespace
+#   }
 # }
